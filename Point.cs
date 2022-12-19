719910cfb5d65eba4d175359fb3ebae2b84148fc
Ezdies <bvbclubbvb@gmail.com>
2022-12-19 11:11:07 +0100

    Init commit

---
+++ Point.cs
@@ -0,0 +1,9 @@
+using System.Runtime.InteropServices;
+namespace Basic{
+    public class Point{
+        public int X {get;}
+        public int Y {get;}
+
+        public Point(int x, int y) => (X, Y) = (x, y);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿
+
+namespace Basic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var xString = Console.ReadLine();
+            var yString = Console.ReadLine();
+
+
+            if (!IsValidInput(xString))
+                return;
+
+            if (!IsValidInput(yString))
+                return;
+
+
+            /*
+
+                        var charArray = xString.ToCharArray();
+
+
+
+                        var charsInYString = new Stack<char>(yString.ToCharArray());
+                        while (charsInYString.Count > 0)
+                        {
+                            var c = charsInYString.Pop();
+                            if (!Char.IsDigit(c))
+                            {
+                                Console.WriteLine("Podano conajmniej jeden znak, który nie jest cyfrą");
+                                return;
+                            }
+                        }
+
+                    */
+            var x = int.Parse(xString!);
+            var y = int.Parse(yString!);
+
+            var p = new Point(x, y);
+            Console.WriteLine(p.X);
+            Console.WriteLine(yString);
+
+        }
+
+        public static bool IsStringNullOrEmpy(string? input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+
+        public static bool DoesContainOnlyDigits(string? input)
+        {
+            var list = new List<char>(input!.ToCharArray());
+
+            foreach (var c in list)
+            {
+                if (!Char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+        public static bool IsValidInput(string? input)
+        {
+            return !IsStringNullOrEmpy(input) && DoesContainOnlyDigits(input);
+        }
+    }
+
+}
